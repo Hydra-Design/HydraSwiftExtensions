1fc527f7e9bfdd65ad83e7108673bb1f72df89d1
Lukas Simonson <lukas@hydradesignllc.com>
2022-04-27 16:01:23 -0600

    Update HydraSwiftExtensionsTests.swift

--- Tests/HydraSwiftExtensionsTests/HydraSwiftExtensionsTests.swift
+++ Tests/HydraSwiftExtensionsTests/HydraSwiftExtensionsTests.swift
@@ -6,6 +6,6 @@ final class HydraSwiftExtensionsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(HydraSwiftExtensions().text, "Hello, World!")
+        //XCTAssertEqual(HydraSwiftExtensions().text, "Hello, World!")
     }
 }
