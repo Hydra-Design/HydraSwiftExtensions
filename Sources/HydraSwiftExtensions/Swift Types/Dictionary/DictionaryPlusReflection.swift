4978e30220f7298165810ad84b8a7e7bf67a6ab2
Lukas Simonson <lukas@hydradesignllc.com>
2022-06-13 08:26:53 -0600

    Added Reflecting Dictionary

--- Sources/HydraSwiftExtensions/Swift Types/Dictionary/DictionaryPlusReflection.swift
+++ Sources/HydraSwiftExtensions/Swift Types/Dictionary/DictionaryPlusReflection.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension Dictionary where Key == String, Value == Any {
+    
+    /// Creates a `Dictionary<String, Any>` that represents the given object.
+    init( reflecting object : Any ) {
+        
+        let mirror = Mirror(reflecting: object)
+        var dict = [ String : Any ]()
+        
+        mirror.children.forEach { child in
+            if let attrName = child.label {
+                dict[ attrName ] = child.value
+            }
+        }
+        
+        self = dict
+    }
+}
