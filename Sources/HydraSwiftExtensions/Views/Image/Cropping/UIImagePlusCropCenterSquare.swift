acf55c8b270862d0f1237198e291c7580ff444fe
Lukas Simonson <lukas@hydradesignllc.com>
2022-06-22 10:15:16 -0600

    Some Update

--- Sources/HydraSwiftExtensions/Views/Image/Cropping/UIImagePlusCropCenterSquare.swift
+++ Sources/HydraSwiftExtensions/Views/Image/Cropping/UIImagePlusCropCenterSquare.swift
@@ -12,6 +12,10 @@ import UIKit
 
 extension UIImage {
 	
+    /// Crops the UIImage into a Square, cutting off excess in either the width or height.
+    ///
+    /// - Version: Beta 0.1
+    @available( *, deprecated, message: "Can cause Crashes", renamed: "cropImageToSquare()" )
 	public func cropImageToSquare() -> UIImage? {
 		
 		var imageHeight = self.size.height
@@ -36,5 +40,42 @@ extension UIImage {
 		
 		return nil
 	}
+    
+    /// Crops the UIImage into a Square, cutting off excess in either the width or height.
+    ///
+    /// - Version: Beta 0.2
+    public func cropImageToSquare() throws -> UIImage {
+        
+        var imageHeight = self.size.height
+        var imageWidth = self.size.width
+        
+        if imageHeight > imageWidth { imageHeight = imageWidth }
+        else { imageWidth = imageHeight }
+        
+        let size = CGSize( width: imageWidth, height: imageHeight )
+        
+        if let CGImage = self.cgImage {
+            let refWidth = CGFloat( CGImage.width )
+            let refHeight = CGFloat( CGImage.height )
+            
+            let x = ( refWidth - size.width ) / 2
+            let y = ( refHeight - size.height ) / 2
+            
+            let cropRect = CGRect( x: x, y: y, width: size.width, height: size.height )
+            
+            if let imageRef = CGImage.cropping( to: cropRect ) {
+                return UIImage( cgImage: imageRef, scale: 0, orientation: self.imageOrientation )
+            } else {
+                throw
+            }
+        } else {
+            throw
+        }
+    }
+    
+    
+    enum HSWUIImageError : Error {
+        case couldntAccessCGImage
+    }
 }
 #endif
