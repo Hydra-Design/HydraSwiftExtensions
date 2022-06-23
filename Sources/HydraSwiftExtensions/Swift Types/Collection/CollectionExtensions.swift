b01590a1d87d016723f0e00c781c849ddec27a73
Lukas Simonson <lukas@hydradesignllc.com>
2022-06-23 10:21:16 -0600

    Added Paul Hegarty Collection Extensions

---
+++ Sources/HydraSwiftExtensions/Swift Types/Collection/CollectionExtensions.swift
@@ -0,0 +1,48 @@
+//
+//  CollectionExtensions.swift
+//  
+//
+//  Created by Paul Hegarty.
+//  Documented by Lukas Simonson.
+//
+
+import Foundation
+
+extension Collection where Element: Identifiable {
+    
+    /// Gets the index of the first item matching the given item.
+    ///
+    /// - Returns: An Optional `Index` value.
+    ///
+    /// - Version: 1.0
+    func index(matching element: Element) -> Self.Index? {
+        firstIndex(where: { $0.id == element.id })
+    }
+}
+
+extension RangeReplaceableCollection where Element: Identifiable {
+    
+    /// Removes the given item from this `Array`
+    ///
+    /// - Version: 1.0
+    mutating func remove(_ element: Element) {
+        if let index = index(matching: element) {
+            remove(at: index)
+        }
+    }
+    
+    subscript(_ element: Element) -> Element {
+        get {
+            if let index = index(matching: element) {
+                return self[index]
+            } else {
+                return element
+            }
+        }
+        set {
+            if let index = index(matching: element) {
+                replaceSubrange(index...index, with: [newValue])
+            }
+        }
+    }
+}
