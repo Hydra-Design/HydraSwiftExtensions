1b19a4605946f88dcd63bd65f7cd20a5218eeeb6
Lukas Simonson <lukas@hydradesignllc.com>
2022-06-29 09:43:07 -0600

    Removed Depracated cropImageToSquare()

--- Sources/HydraSwiftExtensions/Views/Image/Cropping/UIImagePlusCropCenterSquare.swift
+++ Sources/HydraSwiftExtensions/Views/Image/Cropping/UIImagePlusCropCenterSquare.swift
@@ -11,36 +11,6 @@ import Foundation
 import UIKit
 
 extension UIImage {
-	
-    /// Crops the UIImage into a Square, cutting off excess in either the width or height.
-    ///
-    /// - Version: Beta 0.1
-    ///
-    @available( *, deprecated, message: "Can cause Crashes", renamed: "cropImageToSquare()" )
-	public func cropImageToSquare() -> UIImage? {
-		
-		var imageHeight = self.size.height
-		var imageWidth = self.size.width
-		
-		if imageHeight > imageWidth {
-			imageHeight = imageWidth
-		} else { imageWidth = imageHeight }
-		
-		let size = CGSize( width: imageWidth, height: imageHeight )
-		
-		let refWidth : CGFloat = CGFloat( self.cgImage!.width )
-		let refHeight : CGFloat = CGFloat( self.cgImage!.height )
-		
-		let x = ( refWidth - size.width ) / 2
-		let y = ( refHeight - size.height ) / 2
-		
-		let cropRect = CGRect( x: x, y: y, width: size.height, height: size.width )
-		if let imageRef = self.cgImage!.cropping( to: cropRect ) {
-			return UIImage( cgImage: imageRef, scale: 0, orientation: self.imageOrientation )
-		}
-		
-		return nil
-	}
     
     /// Crops the UIImage into a Square, cutting off excess in either the width or height.
     ///
