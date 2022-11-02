96a2d793a5c197985a54194cb515360d95210bc6
Lukas Simonson <lukas@hydradesignllc.com>
2022-11-02 09:27:18 -0600

    Adds Star and Shape.style
    Adds:
      Star - A Shape that allows the user to make a whole bunch  of different shapes.
      Shape.style() - A method that allows us to both stroke and fill a Shape.

---
+++ Sources/HydraSwiftExtensions/Views/Shape/ShapePlusStyle.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas Simonson on 11/1/22.
+//
+
+import SwiftUI
+
+@available(macOS 10.15, *)
+extension Shape {
+    
+    /// Styles this `Shape` with the given stroke and fill.
+    ///
+    /// - Parameters:
+    ///   - stroke: A `ShapeStyle` conforming object to use as the Stroke / Border of this Shape.
+    ///   - strokeWidth: A `CGFloat` that controls the thickness of this Shapes Stroke / Border.
+    ///   - fill: A `ShapeStyle` conforming object to use as the fill of this Shape.
+    ///
+    /// - Version:
+    ///   1.0
+    ///
+    /// - Returns:
+    ///   `some View`
+    func style (
+        stroke: some ShapeStyle,
+        strokeWidth: CGFloat,
+        fill fillContent: some ShapeStyle
+    ) -> some View {
+        self
+            .stroke(stroke, lineWidth: strokeWidth)
+            .background(fill(fillContent))
+    }
+}
