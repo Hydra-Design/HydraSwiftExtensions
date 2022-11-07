eec700f41bce409e6e1b662ba00b50630c63db21
Lukas Simonson <lukas@hydradesignllc.com>
2022-11-07 16:17:33 -0700

    Adds the matching functionality to Arrays and Collections of Equatable.

--- Sources/HydraSwiftExtensions/SwiftTypes/Collection/CollectionExtensions.swift
+++ Sources/HydraSwiftExtensions/SwiftTypes/Collection/CollectionExtensions.swift
@@ -8,9 +8,47 @@
 
 import Foundation
 
-@available(macOS 10.15, *)
-@available( iOS 13.0, * )
-extension Collection where Element: Identifiable {
+public extension Collection where Element: Equatable {
+
+    func indicies(matching element: Element) -> [ Self.Index ] {
+        var matchingIndicies = [Self.Index]()
+        
+        for index in self.indices {
+            if self[index] == element {
+                matchingIndicies.append(index)
+            }
+        }
+        
+        return matchingIndicies
+    }
+}
+
+public extension RangeReplaceableCollection where Element: Equatable {
+    
+    mutating func removeMatching( _ element: Element ) {
+        for index in self.indicies(matching: element) {
+            remove(at: index)
+        }
+    }
+    
+    subscript( _ element: Element ) -> [Element] {
+        get {
+            var elements = [Element]()
+            for indicy in self.indicies(matching: element) {
+                elements.append(self[indicy])
+            }
+            return elements
+        }
+        set {
+            for indicy in self.indicies(matching: element) {
+                replaceSubrange(indicy...indicy, with: [element])
+            }
+        }
+    }
+}
+
+@available( iOS 13.0, macOS 10.15, * )
+public extension Collection where Element: Identifiable {
     
     /// Gets the index of the first item matching the given item.
     ///
@@ -22,9 +60,8 @@ extension Collection where Element: Identifiable {
     }
 }
 
-@available(macOS 10.15, *)
-@available( iOS 13.0, * )
-extension RangeReplaceableCollection where Element: Identifiable {
+@available( iOS 13.0, macOS 10.15, * )
+public extension RangeReplaceableCollection where Element: Identifiable {
     
     /// Removes the given item from this `Array`
     ///
